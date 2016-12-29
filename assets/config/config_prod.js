20e95ca752d31f941f491cd3eb5622549ff9423c
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2016-12-29 17:41:36 +0000

    new web version

---
+++ assets/config/config_prod.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration settings according to environment
+ */
+var config = {
+  environment: 'production',
+  accessToken: 'a874fbc88fe746b7a9bcd0b1f52738b2',
+  heapId: "3561700961",
+  googleAnalytics: 'UA-46207216-1'
+};
